cc5cb18516073b78e0d8e9a074612e1de7d37532
NasifaTalukder <nasifatldr36@gmail.com>
2023-04-03 20:19:45 -0500

    cartoon image lightBox

---
+++ js/main.js
@@ -0,0 +1,74 @@
+let image1=document.querySelector(".img1");
+let image2=document.querySelector(".img2");
+let image3=document.querySelector(".img3");
+let image4=document.querySelector(".img4");
+let image5=document.querySelector(".img5");
+let image6=document.querySelector(".img6");
+let image7=document.querySelector(".img7");
+let image8=document.querySelector(".img8");
+let imgShow_wrap=document.querySelector(".imgShow_wrapOne");
+let imgShow_wrap2=document.querySelector(".imgShow_wrapTwo");
+let imgShow_wrap3=document.querySelector(".imgShow_wrapTre");
+let imgShow_wrap4=document.querySelector(".imgShow_wrapFour");
+let imgShow_wrap5=document.querySelector(".imgShow_wrapFive");
+let imgShow_wrap6=document.querySelector(".imgShow_wrapSix");
+let imgShow_wrap7=document.querySelector(".imgShow_wrapSeven");
+let imgShow_wrap8=document.querySelector(".imgShow_wrapEight");
+let showWrap=document.querySelector("showWrapOne");
+let showWrap2=document.querySelector("showWrapTwo");
+let showWrap3=document.querySelector("showWrapTre");
+let showWrap4=document.querySelector("showWrapFour");
+let showWrap5=document.querySelector("showWrapFive");
+let showWrap6=document.querySelector("showWrapSix");
+let showWrap7=document.querySelector("showWrapSeven");
+let showWrap8=document.querySelector("showWrapEight");
+
+
+let img1Func=()=>{
+    imgShow_wrap.classList.add("showWrapOne");
+}
+let img1HideFunc=()=>{
+    imgShow_wrap.classList.remove("showWrapOne")
+}
+let img2Func=()=>{
+    imgShow_wrap2.classList.add("showWrapTwo");
+}
+let img2HideFunc=()=>{
+    imgShow_wrap2.classList.remove("showWrapTwo")
+}
+let img3Func=()=>{
+    imgShow_wrap3.classList.add("showWrapTre");
+}
+let img3HideFunc=()=>{
+    imgShow_wrap3.classList.remove("showWrapTre")
+}
+let img4Func=()=>{
+    imgShow_wrap4.classList.add("showWrapFour");
+}
+let img4HideFunc=()=>{
+    imgShow_wrap4.classList.remove("showWrapFour")
+}
+let img5Func=()=>{
+    imgShow_wrap5.classList.add("showWrapFive");
+}
+let img5HideFunc=()=>{
+    imgShow_wrap5.classList.remove("showWrapFive")
+}
+let img6Func=()=>{
+    imgShow_wrap6.classList.add("showWrapSix");
+}
+let img6HideFunc=()=>{
+    imgShow_wrap6.classList.remove("showWrapSix")
+}
+let img7Func=()=>{
+    imgShow_wrap7.classList.add("showWrapSeven");
+}
+let img7HideFunc=()=>{
+    imgShow_wrap7.classList.remove("showWrapSeven")
+}
+let img8Func=()=>{
+    imgShow_wrap8.classList.add("showWrapEight");
+}
+let img8HideFunc=()=>{
+    imgShow_wrap8.classList.remove("showWrapEight")
+}
