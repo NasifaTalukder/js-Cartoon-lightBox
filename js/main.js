5c82ed286e277d1120721ce0c2484e8847327d7a
NasifaTalukder <nasifatldr36@gmail.com>
2023-04-09 10:46:07 -0500

    last update

--- js/main.js
+++ js/main.js
@@ -22,53 +22,82 @@ let showWrap5=document.querySelector("showWrapFive");
 let showWrap6=document.querySelector("showWrapSix");
 let showWrap7=document.querySelector("showWrapSeven");
 let showWrap8=document.querySelector("showWrapEight");
+let crossBtn=document.querySelector(".crossBtn");
+let crossBtn2=document.querySelector(".crossBtn2");
+let crossBtn3=document.querySelector(".crossBtn3");
+let crossBtn4=document.querySelector(".crossBtn4");
+let crossBtn5=document.querySelector(".crossBtn5");
+let crossBtn6=document.querySelector(".crossBtn6");
+let crossBtn7=document.querySelector(".crossBtn7");
+let crossBtn8=document.querySelector(".crossBtn8");
+let overlay=document.querySelector(".overlay");
+let overlayShow=document.querySelector(".overlayShow")
+
+
 
 
 let img1Func=()=>{
     imgShow_wrap.classList.add("showWrapOne");
+    overlay.classList.add("overlayShow")
 }
-let img1HideFunc=()=>{
-    imgShow_wrap.classList.remove("showWrapOne")
-}
+crossBtn.addEventListener("click",()=>{
+    imgShow_wrap.classList.remove("showWrapOne");
+    overlay.classList.remove("overlayShow")
+})
+
 let img2Func=()=>{
     imgShow_wrap2.classList.add("showWrapTwo");
+    overlay.classList.add("overlayShow")
 }
-let img2HideFunc=()=>{
-    imgShow_wrap2.classList.remove("showWrapTwo")
-}
+crossBtn2.addEventListener("click",()=>{
+    imgShow_wrap2.classList.remove("showWrapTwo");
+    overlay.classList.remove("overlayShow")
+})
 let img3Func=()=>{
     imgShow_wrap3.classList.add("showWrapTre");
+    overlay.classList.add("overlayShow")
 }
-let img3HideFunc=()=>{
-    imgShow_wrap3.classList.remove("showWrapTre")
-}
+crossBtn3.addEventListener("click",()=>{
+    imgShow_wrap3.classList.remove("showWrapTre");
+    overlay.classList.remove("overlayShow")
+})
 let img4Func=()=>{
     imgShow_wrap4.classList.add("showWrapFour");
+    overlay.classList.add("overlayShow")
 }
-let img4HideFunc=()=>{
-    imgShow_wrap4.classList.remove("showWrapFour")
-}
+crossBtn4.addEventListener("click",()=>{
+    imgShow_wrap4.classList.remove("showWrapFour");
+    overlay.classList.remove("overlayShow")
+})
 let img5Func=()=>{
     imgShow_wrap5.classList.add("showWrapFive");
+    overlay.classList.add("overlayShow")
 }
-let img5HideFunc=()=>{
-    imgShow_wrap5.classList.remove("showWrapFive")
-}
+crossBtn5.addEventListener("click",()=>{
+    imgShow_wrap5.classList.remove("showWrapFive");
+    overlay.classList.remove("overlayShow")
+})
 let img6Func=()=>{
     imgShow_wrap6.classList.add("showWrapSix");
+    overlay.classList.add("overlayShow")
 }
-let img6HideFunc=()=>{
-    imgShow_wrap6.classList.remove("showWrapSix")
-}
+crossBtn6.addEventListener("click",()=>{
+    imgShow_wrap6.classList.remove("showWrapSix");
+    overlay.classList.remove("overlayShow")
+})
 let img7Func=()=>{
     imgShow_wrap7.classList.add("showWrapSeven");
+    overlay.classList.add("overlayShow")
 }
-let img7HideFunc=()=>{
-    imgShow_wrap7.classList.remove("showWrapSeven")
-}
+crossBtn7.addEventListener("click",()=>{
+    imgShow_wrap7.classList.remove("showWrapSeven");
+    overlay.classList.remove("overlayShow")
+})
 let img8Func=()=>{
     imgShow_wrap8.classList.add("showWrapEight");
+    overlay.classList.add("overlayShow")
 }
-let img8HideFunc=()=>{
-    imgShow_wrap8.classList.remove("showWrapEight")
-}
+crossBtn8.addEventListener("click",()=>{
+    imgShow_wrap8.classList.remove("showWrapEight");
+    overlay.classList.remove("overlayShow")
+})
